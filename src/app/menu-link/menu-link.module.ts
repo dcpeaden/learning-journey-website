40467721210e279018ae3906b3349a978c02f009
Cole Peaden <dcpeaden@gmail.com>
2022-01-25 22:12:47 -0600

    fix module errors (#2)
    * add menu link module to app module
    
    * add component declaration and menu import

--- src/app/menu-link/menu-link.module.ts
+++ src/app/menu-link/menu-link.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbIconModule } from '@nebular/theme';
+import { NbIconModule, NbMenuModule } from '@nebular/theme';
 import { MenuLinkComponent } from './menu-link.component';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MenuLinkComponent],
   imports: [
     CommonModule,
     NbIconModule,
     NbEvaIconsModule,
+    NbMenuModule
   ],
   exports: [MenuLinkComponent],
 })
